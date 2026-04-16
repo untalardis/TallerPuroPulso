5636b71726be2e090c7ff157212cc42989ec7e89
untalardis <anndress818@gmail.com>
2026-04-15 19:48:37 -0500

    fix: react router basename para gh pages sub-routing

--- src/App.jsx
+++ src/App.jsx
@@ -12,7 +12,7 @@ function App() {
   const [count, setCount] = useState(0);
 
   return (
-    <BrowserRouter>
+    <BrowserRouter basename="/TallerPuroPulso">
       <Navbar />
       <Routes>
         <Route path="/" element={<Home />} />
